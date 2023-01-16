1bf22a7f8d6117c0d3eef2f83120616ff15825bf
neirolex <13675091+neirolex@users.noreply.github.com>
2023-01-16 17:03:44 +0300

    DI implemented

--- Program.cs
+++ Program.cs
@@ -1,59 +1,38 @@
 ﻿using discord_music_bot.discord;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace discord_music_bot {
     public class Program
     {
+        private readonly IServiceProvider _services;
+
+        public Program()
+        {
+            _services = CreateProvider();
+        }
+
+        static IServiceProvider CreateProvider()
+        {
+            var collection = new ServiceCollection()
+                .AddSingleton<DiscordClient>()
+                .AddSingleton<Player>();
+
+            return collection.BuildServiceProvider();
+        }
+
+        // Program entry point
         public static Task Main(string[] args) => new Program().MainAsync();
 
         public async Task MainAsync()
         {
-            //InitializeMusicPlayer();
-            InitializeDiscordBot();
+            //Initialize discorde client
+            var discordClient = _services.GetService<DiscordClient>();
+
+            //Initialize music player
+            var player = _services.GetService<Player>();
 
             // Block this task until the program is closed.
             await Task.Delay(Timeout.Infinite);
-        }
-
-        private void InitializeDiscordBot() {
-            Client c = new Client();
-            c.Init();
-        }
-
-        private void InitializeMusicPlayer() {
-            Player p = new Player();
-
-            AudioFile track1 = new AudioFile("C:\\_work\\_repo\\discord-music-bot\\tracks\\track1.mp3");
-            p.AddToQueue(track1);
-
-            AudioFile track2 = new AudioFile("C:\\_work\\_repo\\discord-music-bot\\tracks\\track2.mp3");
-            p.AddToQueue(track2);
-
-            while(true) {
-                var command = Console.ReadLine();
-
-                switch(command) {
-                    case "play": {
-                        p.Play();
-                        break;
-                    }
-                    case "pause": {
-                        p.Pause();
-                        break;
-                    }
-                    case "stop": {
-                        p.Stop();
-                        break;
-                    }
-                    case "next": {
-                        p.Next();
-                        break;
-                    }
-                    case "prev": {
-                        p.Prev();
-                        break;
-                    }
-                }
-            }
         }
     }
 }
--- discord/Client.cs
+++ discord/Client.cs
@@ -1,19 +1,37 @@
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
 
 namespace discord_music_bot.discord
 {
-    public class Client
+    public class DiscordClient
     {
-        private DiscordSocketClient _client;
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
         string _token = "MTA2NDUyNzMwNDE0NDk4NjEzNA.GLBAc2.28OyhM6NNTSqcJQ2Ef1SCG4X9w7iuvF7a8BO4E";
 
-        public Client() { 
-            _client = new DiscordSocketClient();
+        public DiscordClient() { 
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+            });
+            
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Info,
+                CaseSensitiveCommands = false,
+            });
+            
+            // Subscribe the logging handler to both the client and the CommandService.
             _client.Log += Log;
+            _commands.Log += Log;
+
+            //Start bot
+            Init();
         }
 
-        public async void Init() {
+        private async void Init() {
             await _client.LoginAsync(TokenType.Bot, _token);
             await _client.StartAsync();
         }
