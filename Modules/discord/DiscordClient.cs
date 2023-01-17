e2b50759c2f1b664dffe269da429346f333b3fbe
neirolex <13675091+neirolex@users.noreply.github.com>
2023-01-17 19:11:13 +0300

    DiscordClient initialization moved from Program.cs

--- Modules/discord/DiscordClient.cs
+++ Modules/discord/DiscordClient.cs
@@ -1,14 +1,80 @@
-using System.Diagnostics;
-
-using Discord.Audio;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace discord_music_bot
 {
     public class DiscordClient
     {
+        private DiscordSocketClient _client;
+        private InteractionService _interactionService;
+        private CommandHandler _commandHandler;
+        private ulong _guildId;
         public DiscordAudioService AudioService; //TODO: Incapslate, make private
-        public DiscordClient(DiscordAudioService audioService) {
+        public DiscordClient(
+                DiscordAudioService audioService, 
+                DiscordSocketClient client, 
+                InteractionService interactionService, 
+                CommandHandler commandHandler) {
             AudioService = audioService;
+            _client = client;
+            _interactionService = interactionService;
+            _commandHandler = commandHandler;
+
+            _guildId = ulong.Parse("706622645105328168");
+
+            //Logging
+            _client.Log += LogAsync;
+            _interactionService.Log += LogAsync;
+
+            //Do when ready
+            _client.Ready += ReadyAsync;
+        }
+
+        public async Task Init() {
+            var token = "MTA2NDUyNzMwNDE0NDk4NjEzNA.G2elQs._r-AtI5BCRcUpph4LPZ8GS2qXTDDxfFhIUyP3w";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Initialize the CommandHandler service
+            await _commandHandler.InitializeAsync();
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task ReadyAsync()
+        {
+            // if (IsDebug())
+            // {
+            //     System.Console.WriteLine($"In debug mode, adding commands to {_guildId}...");
+            //     await _commands.RegisterCommandsToGuildAsync(_guildId);
+            // }
+            // else
+            // {
+            //     // this method will add commands globally, but can take around an hour
+            //     await _commands.RegisterCommandsGloballyAsync(true);
+            // }
+
+            //Loading commands to the server (localy)
+            System.Console.WriteLine($"Adding commands to {_guildId}...");
+            await _interactionService.RegisterCommandsToGuildAsync(_guildId);
+            Console.WriteLine($"Bot successfully connected to the server!");
+        }
+
+        static bool IsDebug ( )
+        {
+            #if DEBUG
+                return true;
+            #else
+                return false;
+            #endif
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Discord;
-using Discord.Interactions;
+﻿using Discord.Interactions;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,9 +9,6 @@ class Program
     {
         // setup our fields we assign later
         private readonly IConfiguration _config;
-        private DiscordSocketClient _client;
-        private InteractionService _commands;
-        private ulong _guildId;
 
         public Program()
         {
@@ -22,62 +18,18 @@ class Program
                 //.AddJsonFile(path: "config.json");  
       
             _config = _builder.Build();
-            _guildId = ulong.Parse("706622645105328168");
         }
 
         public static Task Main(string[] args) => new Program().MainAsync();
 
         public async Task MainAsync()
         {
-             using (var services = ConfigureServices())
+            using (var services = ConfigureServices())
             {
-                //Client and command module initialization
-                var client = services.GetRequiredService<DiscordSocketClient>();
-                var commands = services.GetRequiredService<InteractionService>();
-                _client = client;
-                _commands = commands;
-
-                //Logging
-                client.Log += LogAsync;
-                commands.Log += LogAsync;
-
-                //Do when ready
-                client.Ready += ReadyAsync;
-
-                //Login and start bot
-                await client.LoginAsync(TokenType.Bot, "MTA2NDUyNzMwNDE0NDk4NjEzNA.G2elQs._r-AtI5BCRcUpph4LPZ8GS2qXTDDxfFhIUyP3w");
-                await client.StartAsync();
-
-                // Initialize the CommandHandler service
-                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+                await services.GetRequiredService<DiscordClient>().Init();
 
                 await Task.Delay(Timeout.Infinite);
             }
-        }
-
-        private Task LogAsync(LogMessage log)
-        {
-            Console.WriteLine(log.ToString());
-            return Task.CompletedTask;
-        }
-
-        private async Task ReadyAsync()
-        {
-            // if (IsDebug())
-            // {
-            //     System.Console.WriteLine($"In debug mode, adding commands to {_guildId}...");
-            //     await _commands.RegisterCommandsToGuildAsync(_guildId);
-            // }
-            // else
-            // {
-            //     // this method will add commands globally, but can take around an hour
-            //     await _commands.RegisterCommandsGloballyAsync(true);
-            // }
-
-            //Loading commands to the server (localy)
-            System.Console.WriteLine($"Adding commands to {_guildId}...");
-            await _commands.RegisterCommandsToGuildAsync(_guildId);
-            Console.WriteLine($"Bot successfully connected to the server!");
         }
 
         //Dependency Injection - load services
@@ -90,17 +42,13 @@ class Program
                 .AddSingleton<DiscordSocketClient>()
                 .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
                 .AddSingleton<CommandHandler>()
-                .AddSingleton(x => new DiscordClient(x.GetRequiredService<DiscordAudioService>()))
+                .AddSingleton(x => new DiscordClient(
+                    x.GetRequiredService<DiscordAudioService>(), 
+                    x.GetRequiredService<DiscordSocketClient>(), 
+                    x.GetRequiredService<InteractionService>(), 
+                    x.GetRequiredService<CommandHandler>()
+                    ))
                 .BuildServiceProvider();
-        }
-
-        static bool IsDebug ( )
-        {
-            #if DEBUG
-                return true;
-            #else
-                return false;
-            #endif
         }
     }
 }
